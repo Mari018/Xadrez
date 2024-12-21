dc78abb7b443ddda232bd7b02aab7e577e67c486
Mari018 <andrepinto695@gmail.com>
2024-12-21 19:29:01 +0000

    player array

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import Game.Game;
+import Game.Player;
 
 public class Main {
     public static void main(String[] args) {
 
-        Player player = new Player("Mário");
-        Player player1 = new Player("Inês");
+        Player[] players = {new Player("Mário"),
+                new Player("Inês")};
 
-        Game game = new Game(player,player1);
+        Game game = new Game(players);
+
     }
 }
