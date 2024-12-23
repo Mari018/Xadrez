37f0914e15b511b7c28e6325bfbf0a73e566815e
Mari018 <andrepinto695@gmail.com>
2024-12-23 11:18:11 +0000

    player array

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -10,7 +10,7 @@ public class Game {
     private Player[] players;
     private Square[][] board;
     private char[] row = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-
+    private int[] col = {1, 2, 3, 4, 5, 6, 7, 8};
 
     public Game(Player[] players) {
 
@@ -109,7 +109,7 @@ public class Game {
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                System.out.print(" " + i + row[j]);
+                System.out.print(" " + col[i] + row[j]);
 
                 Piece piece = board[i][j].getCurrPiece();
                 if (piece == null) {
@@ -137,7 +137,6 @@ public class Game {
                 }
 
             }
-            System.out.println(" " + (i + 1));
 
         }
 
